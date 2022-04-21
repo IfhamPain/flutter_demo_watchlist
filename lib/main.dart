fa8e0ca69163983bb6016c0bf8d7555718be5677
Ifhamm <m.ifham@directfn.com>
2022-04-21 09:50:23 +0530

    Set dataset to 1000

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
-import 'dart:collection';
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
-import 'dart:collection';
 import 'dart:async';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,70 +16,60 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-refreshSymbols () {
-  symList = [
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-    Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  ];
+createList () {
+  symList1 = [];
+
+  for (int i=0; i<1000; i++) {
+    symList1.add({"AAA" + i.toString() : double.parse((Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00).toStringAsFixed(2))});
+  }
 }
 
-List<Symbols> symList = [
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-  Symbols(symCode: "AAA", price: Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00),
-];
+List <Map<String, double>> symList1 = [];
 
-class Symbols {
+class Symbol {
   String symCode;
   double price;
 
-  Symbols({required this.symCode, required this.price});
+  Symbol({required this.symCode, required this.price});
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   _MyHomePageState createState() {
     return _MyHomePageState();
   }
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   late TabController _controller;
-  int _selectedIndex = 0;
 
   @override
   void initState() {
     _controller = TabController(length: 2, vsync: this);
 
     _controller.addListener(() {
-      setState(() {
-        _selectedIndex = _controller.index;
-      });
+      setState(() {});
       // Do whatever you want based on the tab index
     });
-
+    Timer.periodic(const Duration(milliseconds: 200), (timer) {
+      setState(() {
+        createList();
+      });
+    });
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -91,9 +79,11 @@ class _MyHomePageState extends State<MyHomePage>
         length: 2,
         child: Scaffold(
           appBar: AppBar(
-            backgroundColor: Color(0xFFFFFFFF),
-            title: Center(
-              child: Text("UA MOB Flutter", style: TextStyle(color: Colors.black),
+            backgroundColor: const Color(0xFFFFFFFF),
+            title: const Center(
+              child: Text(
+                "UA MOB Flutter",
+                style: TextStyle(color: Colors.black),
               ),
             ),
           ),
@@ -110,42 +100,32 @@ class _MyHomePageState extends State<MyHomePage>
   }
 
   Widget menu() {
-    return Container(
-      child: const TabBar(
-        tabs: [
-          Tab(
-            child: Text(
-              'Watch 1',
-                style: TextStyle(
-                  color: Colors.black,
-                ),
+    return const TabBar(
+      tabs: [
+        Tab(
+          child: Text(
+            'Watch 1',
+            style: TextStyle(
+              color: Colors.black,
             ),
           ),
-          Tab(
-            child: Text(
-              'Watch 2',
-              style: TextStyle(
-                color: Colors.black,
-              ),
+        ),
+        Tab(
+          child: Text(
+            'Watch 2',
+            style: TextStyle(
+              color: Colors.black,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
   Widget watchList() {
-    Timer.periodic(const Duration(milliseconds: 2000), (timer) {
-      setState(() {
-        refreshSymbols();
-      });
-    });
     return GridView.builder(
-      itemCount: symList.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 1.5
-      ),
+      itemCount: symList1.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 1.5),
       itemBuilder: (BuildContext context, int index) {
         return GridView.count(
           crossAxisCount: 1,
@@ -156,13 +136,11 @@ class _MyHomePageState extends State<MyHomePage>
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
-              child: Container(
-                child: Column(
-                  children: [
-                      Text(symList[index].symCode),
-                      Text(symList[index].price.toString()),
-                  ],
-                ),
+              child: Column(
+                children: [
+                  Text(symList1[index].keys.first),
+                  Text(symList1[index].values.first.toStringAsFixed(2)),
+                ],
               ),
             ),
           ],
