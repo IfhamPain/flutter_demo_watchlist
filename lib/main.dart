733a2b163016587d2f2ef671d0f09f0c8b3dbd64
Ifhamm <m.ifham@directfn.com>
2022-05-04 00:37:43 +0530

    Add Splash Screen. Change list architecture.

--- lib/main.dart
+++ lib/main.dart
@@ -16,26 +16,76 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(),
+      home: Splash(),
     );
   }
 }
 
+// createList () {
+//   symList1 = [];
+//
+//   for (int i=0; i<100; i++) {
+//     symList1.add({"A" + i.toString() : double.parse((Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00).toStringAsFixed(2))});
+//   }
+// }
+
 createList () {
   symList1 = [];
 
-  for (int i=0; i<1000; i++) {
-    symList1.add({"A" + i.toString() : double.parse((Random().nextDouble() * (10.00 - 1.00 + 1.00) + 1.00).toStringAsFixed(2))});
+  for (int i=0; i<100; i++) {
+    symList1.add(DataRow());
   }
 }
 
-List <Map<String, double>> symList1 = [];
+updateList () {
+  if (symList1.isNotEmpty) {
+    for (int i=0; i<symList1.length; i++) {
+      symList1[i].updateNum();
+    }
+  } else {
+    for (int i = 0; i < 100; i++) {
+      symList1.add(DataRow());
+    }
+  }
+}
 
-class Symbol {
-  String symCode;
-  double price;
+List <DataRow> symList1 = [];
 
-  Symbol({required this.symCode, required this.price});
+class DataRow {
+
+  String symCode = "";
+  int num1 = 0;
+  int num2 = 0;
+  int num3 = 0;
+  int num4 = 0;
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  DataRow () {
+    symCode = generateRandomString();
+    num1 = Random().nextInt(900) + 100;
+    num2 = Random().nextInt(900) + 100;
+    num3 = Random().nextInt(900) + 100;
+    num4 = Random().nextInt(900) + 100;
+  }
+
+  updateNum () {
+    num1 = Random().nextInt(900) + 100;
+    num2 = Random().nextInt(900) + 100;
+    num3 = Random().nextInt(900) + 100;
+    num4 = Random().nextInt(900) + 100;
+  }
+}
+
+String generateRandomString() {
+  int length = 4;
+  final _random = Random();
+  const _availableChars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  final randomString = List.generate(length,
+          (index) => _availableChars[_random.nextInt(_availableChars.length)])
+      .join();
+
+  return randomString;
 }
 
 class MyHomePage extends StatefulWidget {
@@ -58,12 +108,13 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
       setState(() {});
       // Do whatever you want based on the tab index
     });
-    createList();
-    // Timer.periodic(const Duration(milliseconds: 200), (timer) {
-    //   setState(() {
-    //
-    //   });
-    // });
+
+    Timer.periodic(const Duration(milliseconds: 200), (timer) {
+      setState(() {
+        updateList();
+      });
+    });
+
     super.initState();
   }
 
@@ -77,13 +128,14 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 2,
+        length: 3,
         child: Scaffold(
           bottomNavigationBar: menu(),
           body: TabBarView(
             children: [
               Container(child: watchList()),
               Container(child: watchList()),
+              Container(child: NativeList())
             ],
           ),
         ),
@@ -110,6 +162,14 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
             ),
           ),
         ),
+        Tab(
+          child: Text(
+            'Native',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
       ],
     );
   }
@@ -117,24 +177,228 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   Widget watchList() {
     return GridView.builder(
       itemCount: symList1.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 13, childAspectRatio: 1.5),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1, childAspectRatio: 26),
       itemBuilder: (BuildContext context, int index) {
         return Flex(
           direction: Axis.horizontal,
           children: [
-            Expanded(
-                child: Column(
-                  children: [
-                    Text(symList1[index].keys.first,
-                      style: const TextStyle(fontSize: 7)),
-                    Text(symList1[index].values.first.toStringAsFixed(2),
-                      style: const TextStyle(fontSize: 7)),
-                  ],
-                )
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 42, 0),
+              child: Text(symList1[index].symCode,
+                  style: const TextStyle(fontSize: 11)),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 42, 0),
+              child: Text(symList1[index].num1.toString(),
+                  style: TextStyle(fontSize: 11, color: symList1[index].num1 < 500 ? Colors.red : Colors.green)),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 42, 0),
+              child: Text(symList1[index].num2.toString(),
+                  style: TextStyle(fontSize: 11, color: symList1[index].num2 < 500 ? Colors.red : Colors.green)),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 42, 0),
+              child: Text(symList1[index].num3.toString(),
+                  style: TextStyle(fontSize: 11, color: symList1[index].num3 < 500 ? Colors.red : Colors.green)),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 42, 0),
+              child: Text(symList1[index].num4.toString(),
+                  style: TextStyle(fontSize: 11, color: symList1[index].num4 < 500 ? Colors.red : Colors.green)),
             ),
           ],
         );
       },
+    );
+  }
+
+  // Widget watchList() {
+  //   return ListView.builder(
+  //     itemCount: 5,
+  //     itemBuilder: (BuildContext context, int index) {
+  //       return Flex(
+  //         direction: Axis.horizontal,
+  //         children: [
+  //           Padding(
+  //             padding: const EdgeInsets.fromLTRB(8, 8, 30, 8),
+  //             child: Text(symList1[index].symCode,
+  //                 style: const TextStyle(fontSize: 16)),
+  //           ),
+  //           Padding(
+  //             padding: const EdgeInsets.fromLTRB(8, 8, 30, 8),
+  //             child: Text(symList1[index].toString(),
+  //                 style: const TextStyle(fontSize: 16)),
+  //           ),
+  //           Padding(
+  //             padding: const EdgeInsets.fromLTRB(8, 8, 30, 8),
+  //             child: Text(symList1[index].toString(),
+  //                 style: const TextStyle(fontSize: 16)),
+  //           ),
+  //           Padding(
+  //             padding: const EdgeInsets.fromLTRB(8, 8, 30, 8),
+  //             child: Text(symList1[index].toString(),
+  //                 style: const TextStyle(fontSize: 16)),
+  //           ),
+  //           Padding(
+  //             padding: const EdgeInsets.fromLTRB(8, 8, 30, 8),
+  //             child: Text(symList1[index].toString(),
+  //                 style: const TextStyle(fontSize: 16)),
+  //           ),
+  //         ],
+  //       );
+  //     },
+  //   );
+  // }
+
+
+  var dropdownValue = 'One';
+
+
+  Widget NativeList() {
+    return Padding(
+      padding: const EdgeInsets.all(50.0),
+      child: ListView(
+        children: [
+          DropdownButton<String>(
+            value: dropdownValue,
+            // icon: const Icon(Icons.arrow_downward),
+            elevation: 16,
+            style: const TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
+            items: <String>['One', 'Two', 'Three', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          MyAlert(),
+          DatePickerCustom(),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter a search term',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyAlert extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext contextFromCall) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ElevatedButton(
+        child: Text('Show alert'),
+        onPressed: () {
+          showAlertDialog(contextFromCall);
+        },
+      ),
+    );
+  }
+
+  showAlertDialog(BuildContext context) {
+    // set up the button
+    Widget okButton = TextButton(
+      child: Text("OK"),
+      onPressed: () {Navigator.of(context, rootNavigator: true).pop('dialog'); },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Native Alert"),
+      content: Text("Native Alert Test"),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+}
+
+class DatePickerCustom extends StatelessWidget {
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate) {
+      // setState(() {
+        selectedDate = picked;
+      // });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // Text("${selectedDate.toLocal()}".split(' ')[0]),
+            // SizedBox(height: 20.0,),
+            ElevatedButton(
+              onPressed: () => _selectDate(context),
+              child: Text('Select date'),
+            ),
+          ],
+        );
+  }
+}
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState () {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 2000), () {});
+    Navigator.pushReplacement(this.context, MaterialPageRoute(builder: (context)=> MyHomePage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/splash.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      )
     );
   }
 }
